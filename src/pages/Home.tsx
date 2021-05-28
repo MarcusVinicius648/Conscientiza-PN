618b2ac036e255a30e665c934087c72c2c4b16c9
felippefaeda <felippefaeda@gmail.com>
2021-05-27 21:14:30 -0300

    teste

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -77,7 +77,7 @@ export function Home(){
 
                 <Text style={styles.address}>
                     {userRua} {"\n"}
-                    BAIRRO: {userBairro} {"\n"}
+                    Teste: {userBairro} {"\n"}
                     {userLocalidade} - {userUf}
                 </Text>
             </View>
