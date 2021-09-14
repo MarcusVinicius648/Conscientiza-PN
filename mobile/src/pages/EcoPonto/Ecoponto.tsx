a3c4eb2671b4c3852d5e8a4d3710e3b7151fd3de
felippefaeda <felippefaeda@gmail.com>
2021-09-14 20:17:04 -0300

    Ajuste no Ecoponto

--- mobile/src/pages/EcoPonto/Ecoponto.tsx
+++ mobile/src/pages/EcoPonto/Ecoponto.tsx
@@ -1,11 +1,11 @@
 import React, {useState,useEffect }from 'react';
-import { Alert, Image, SafeAreaView, StyleSheet,Text, TouchableOpacity, View } from 'react-native';
-import { SideBar } from '../../components/SideBar';
+import { Alert, SafeAreaView, StyleSheet,Text, TouchableOpacity, View } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import { useNavigation, useRoute } from '@react-navigation/core';
 import { ScrollView } from 'react-native-gesture-handler';
 import * as Location from 'expo-location';
 import api from '../../server/api';
+import { Feather as Icon } from '@expo/vector-icons';
 
 import fonts from '../../styles/fonts';
 import colors from '../../styles/colors';
@@ -47,14 +47,30 @@ export function Ecoponto() {
                 return;
             }
 
-            const location = await Location.getCurrentPositionAsync({});
-            const {latitude, longitude} = location.coords;
-            
-            setInicialPositions([
-                latitude,
-                longitude
-            ]);
-           
+            let location;
+
+            try {
+                location = await Location.getCurrentPositionAsync({
+                    accuracy: Location.Accuracy.BestForNavigation
+                });
+            } catch {
+                location = await Location.getLastKnownPositionAsync({
+                    maxAge: 5000,
+                    requiredAccuracy: 5000
+                });                              
+            }
+
+            if (location !== null){
+                setInicialPositions([
+                    location.coords.latitude, 
+                    location.coords.longitude
+                ]);
+            } else {
+                setInicialPositions([
+                    -20.41557397807853, 
+                    -42.90842130196299
+                ]);
+            }
         }
         loadPosition();
     },[]);
@@ -78,9 +94,9 @@ export function Ecoponto() {
 
     function handleNavigateToDetail(id: number) {
         navigation.navigate('Detail', { point_id: id });
-      }
+    }
     
-      function handleSelectItem(id: number) {
+    function handleSelectItem(id: number) {
         const alreadySelected = selectedItems.findIndex(item => item === id);
         
         if (alreadySelected >= 0) {
@@ -99,32 +115,54 @@ export function Ecoponto() {
                 activeIconAbout={false}
             />
 
-            <Text style={styles.title}>
-                 Selecione o Ecoponto para mais informações!
-            </Text>
-            <View style={styles.itemsFilter}>
-                <ScrollView 
-                    horizontal 
-                    showsHorizontalScrollIndicator={false}
-                    contentContainerStyle={{ paddingHorizontal: 20 }}
-                >
-                   {items.map(item =>(
-                        <TouchableOpacity
-                            key={String(item.id)}
-                            activeOpacity={0.6}
-                            style={[
-                                styles.cardFilter,
-                                selectedItems.includes(item.id) ? styles.selectedItem : {}
-                            ]}
-                            onPress={()=> handleSelectItem(item.id)}
-                        >
-                            <Text style={styles.titleFilter}>
-                                {item.title}
-                            </Text>
-                        </TouchableOpacity>
-                   ))}
-                </ScrollView>
-            </View>
+            <View style={styles.filterContainer}>
+                <Text style={styles.title}>
+                    Selecione o Ecoponto para mais informações!
+                </Text>
+                
+                <View style={styles.itemsFilter}>
+                    <ScrollView 
+                        horizontal 
+                        showsHorizontalScrollIndicator={false}
+                        contentContainerStyle={{ paddingHorizontal: 20 }}
+                    >
+                    {items.map(item =>(
+                            <TouchableOpacity
+                                key={String(item.id)}
+                                activeOpacity={0.6}
+                                style={[
+                                    styles.cardFilter,
+                                    selectedItems.includes(item.id) ? styles.selectedItem : {}
+                                ]}
+                                onPress={()=> handleSelectItem(item.id)}
+                            >
+                                <Text style={styles.titleFilter}>
+                                    {item.title}
+                                </Text>
+                            </TouchableOpacity>
+                    ))}
+                    </ScrollView>
+                </View>
+
+                { points.length > 0 && (
+                    <View style={styles.footerContainer}>
+                        <View style={styles.marker}>
+                            <View style={styles.mapLocalization}></View> 
+                            <View style={styles.mapLocalizationRegion}>
+                                <Icon
+                                    name="trash-2" 
+                                    size={24} 
+                                    color={colors.white} 
+                                />    
+                            </View>
+                        </View>
+                        <Text style={styles.legend}>
+                            Clique nos pontos sinalizados por este {'\n'} 
+                            ícone para saber mais detalhes.
+                        </Text>
+                    </View>
+                )}                
+            </View> 
 
             <View style={styles.mapContainer}>
                 { initialPositions[0] !== 0 && (
@@ -137,8 +175,18 @@ export function Ecoponto() {
                             longitudeDelta: 0.014,
                         }}
                     >   
+
+                        <Marker
+                            key={String(0)}
+                            coordinate={{ 
+                                latitude:initialPositions[0],
+                                longitude:initialPositions[1], 
+                            }}
+                            style={styles.marker}
+                        />
+
                         {points.map((point)=>(
-                           <Marker
+                            <Marker
                                 key={String(point.id)}
                                 coordinate={{ 
                                     latitude: point.latitude,
@@ -147,71 +195,87 @@ export function Ecoponto() {
                                 style={styles.marker} 
                                 onPress={()=>handleNavigateToDetail(point.id)}
                             >
-                            <View style={styles.arrowDown}></View>   
-                            <View style={styles.mapMarkerContainer}>
-                                <Text style={styles.mapMarkerTitle}>
-                                    ?
-                                </Text>   
-                            </View>
-                        </Marker>
+                                <View style={styles.mapLocalization}></View> 
+                                <View style={styles.mapLocalizationRegion}>
+                                    <Icon
+                                        name="trash-2" 
+                                        size={24} 
+                                        color={colors.white} 
+                                    />    
+                                </View>
+                            </Marker>
                         ))}
                     </MapView>
-            ) }
+                ) }
             </View>
+
+             
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container:{
-        flex: 1,
-        width: '100%',   
+        flex: 1,        
         backgroundColor: colors.background 
     },
+    filterContainer: {
+        marginLeft: 10,
+        marginTop: 15
+    },
+    footerContainer: {
+        flexDirection: 'row'
+    },
+    legend:{
+        marginTop: 10,
+        marginBottom: 10,
+        textAlign: 'left', 
+        fontSize: 14,
+        fontFamily: fonts.complement
+    },
     title:{
-       marginLeft:15,
-       marginTop:15,
-       textAlign:'left', 
-       fontSize:14,
-       fontFamily:fonts.complement
+       textAlign: 'left', 
+       fontSize: 14,
+       fontFamily: fonts.complement
     },
     mapContainer:{
-        width: '98%',
-        height:'65%',
-        borderRadius: 10,
+        flex: 1,
         overflow: 'hidden',
-        marginLeft:3.5,
-        marginBottom:20,
-        
+        margin: 3.5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8        
     },
     map:{
         width: '100%',
         height: '100%',
+        margin: 20
     },
     itemsFilter:{
         flexDirection: 'row',
     },
     cardFilter:{
         backgroundColor: '#fff',
-        borderWidth: 2,
+        borderWidth: 3,
         borderColor: '#eee',
         height: 50,
         width: 120,
         borderRadius: 8,
         paddingHorizontal: 16,
         paddingTop: 13,
-        paddingBottom: 16,
+        paddingBottom: 5,
         marginRight: 8,
-        marginTop:20,
-        marginBottom:10,
+        marginTop:10,
+        marginBottom:8,
         alignItems: 'center',
         justifyContent: 'space-between',
         textAlign: 'center',
     },
     selectedItem: {
+        backgroundColor: '#eee',
         borderColor: colors.green,
-        borderWidth: 2,
-      },
+        borderWidth: 3,
+    },
     titleFilter:{
         fontSize:15,
         fontFamily:fonts.text
@@ -219,36 +283,25 @@ const styles = StyleSheet.create({
     marker:{
         width: 60,
         height: 60,
+        flexDirection: 'column-reverse',
+        alignItems: 'center',
+        backgroundColor: 'transparent'
     },
-    mapMarkerContainer:{
-        width: 50,
-        height: 40,
+    mapLocalizationRegion:{
+        width: 32,
+        height: 32,
         backgroundColor: colors.green,
-        flexDirection: 'column',
         borderRadius: 4,
-        overflow: 'hidden',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'center'
     },
-    arrowDown:{
-        position: 'absolute',
-        marginLeft: 19,
-        marginTop: 38,
-        width: 0,
-        height:0,
-        borderLeftWidth: 12,
+    mapLocalization:{
+        borderLeftWidth: 10,
         borderLeftColor: 'transparent',
-        borderRightWidth: 12,
+        borderRightWidth: 10,
         borderRightColor: 'transparent',
         borderTopColor:colors.green,
-        borderTopWidth: 12
-    },
-    mapMarkerTitle:{
-        flex: 1,
-        fontFamily:fonts.complement,
-        color: colors.white,
-        fontSize: 25,
-        lineHeight: 15,
-        paddingTop:25
-    },
+        borderTopWidth: 10
+    }
 });
 
